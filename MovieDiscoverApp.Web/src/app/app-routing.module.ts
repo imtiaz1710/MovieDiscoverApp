edb5835a52402a8fee786c2d9de62176be7e196f
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-12-19 08:31:49 +0600

    layout, header, boostrap, primeng added

--- MovieDiscoverApp.Web/src/app/app-routing.module.ts
+++ MovieDiscoverApp.Web/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './shared/modules/layout/layout/layout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./shared/modules/layout/layout.module').then(x => x.LayoutModule), component: LayoutComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ MovieDiscoverApp.Web/src/app/shared/modules/layout/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterConstants } from 'src/app/shared/constants/router-constants';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+
+})
+export class HeaderComponent implements OnInit {
+  isDataLoaded: boolean = false;
+  isNavActive: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  goToMovies() {
+    this.isNavActive = !this.isNavActive;
+  }
+
+  goToPeoples() {
+    this.isNavActive = true;
+  }
+}
