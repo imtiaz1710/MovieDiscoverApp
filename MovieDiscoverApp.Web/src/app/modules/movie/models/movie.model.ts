6717556dbaf841867528f3a6007c58f77849c4fa
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-12-21 13:03:58 +0600

    #5 average vote done

--- MovieDiscoverApp.Web/src/app/modules/movie/models/movie.model.ts
+++ MovieDiscoverApp.Web/src/app/modules/movie/models/movie.model.ts
@@ -38,7 +38,7 @@ export interface MovieDetailsViewModel {
     poster_path: string
     production_companies: ProductionCompany[]
     production_countries: ProductionCountry[]
-    release_date: string
+    release_date: Date
     revenue: number
     runtime: number
     spoken_languages: SpokenLanguage[]
