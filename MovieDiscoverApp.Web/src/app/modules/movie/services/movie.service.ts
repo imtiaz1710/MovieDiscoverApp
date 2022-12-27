b49d3509c8ddc601a4bba98e6d504b6a9a1c8657
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-12-27 21:47:47 +0600

    #2 proxy server error fixed

--- MovieDiscoverApp.Web/src/app/modules/movie/services/movie.service.ts
+++ MovieDiscoverApp.Web/src/app/modules/movie/services/movie.service.ts
@@ -3,37 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Genre, MovieDetailsViewModel, MovieResponseModel, MovieViewModel } from '../models/movie.model';
 import { RouterConstants } from 'src/app/shared/constants/router-constants';
-import { BaseService } from 'src/app/shared/services/base-service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService extends BaseService {
+export class MovieService {
   constructor(private http: HttpClient) {
-    super();
   }
-  // TODO: if we use hard coded api then we do not need proxy-conf.json
+
   getMovies(pageNo: number, movieUrlExtensionString: string): Observable<MovieResponseModel>{
     // TODO: make it const
-    let url = `${this.baseUrl}/3/discover/movie?api_key=${RouterConstants.apiKey}&language=en-US&
+    let url = `/api/3/discover/movie?api_key=${RouterConstants.apiKey}&language=en-US&
       sort_by=popularity.desc&page=${pageNo}&with_watch_monetization_types=flatrate${movieUrlExtensionString}`;
 
     return this.http.get<MovieResponseModel>(url.trim());
   }
 
   getMovieDetailsById(movieId: string): Observable<MovieDetailsViewModel>{
-    return this.http.get<MovieDetailsViewModel>(`${this.baseUrl}/3/movie/${movieId}?api_key=${RouterConstants.apiKey}&language=en-US`);
+    return this.http.get<MovieDetailsViewModel>(`/api/3/movie/${movieId}?api_key=${RouterConstants.apiKey}&language=en-US`);
   }
 
   // TODO: API_KEY can be added in http_interceptor
   getGenres(): Observable<Genre[]>{
-    return this.http.get<any>(`${this.baseUrl}/3/genre/movie/list?api_key=${RouterConstants.apiKey}&language=en-US`)
+    return this.http.get<any>(`/api/3/genre/movie/list?api_key=${RouterConstants.apiKey}&language=en-US`)
       .pipe(map(res => res.genres));
   }
 
   searchMovie(pageNo:number, searchText: string): Observable<MovieViewModel[]>{
     return this.http.get<MovieResponseModel>
-      (`${this.baseUrl}/3/search/movie?api_key=${RouterConstants.apiKey}&language=en-US&query=${searchText}&page=${pageNo}&include_adult=false`)
+      (`/api/3/search/movie?api_key=${RouterConstants.apiKey}&language=en-US&query=${searchText}&page=${pageNo}&include_adult=false`)
         .pipe(map(res => res.results));
   }
 }
--- MovieDiscoverApp.Web/src/app/shared/services/base-service.ts
+++
@@ -1,5 +0,0 @@
-import { environment } from '../../../environments/environment';
-
-export abstract class BaseService {
-  public baseUrl: string = environment.baseUrl;
-}
